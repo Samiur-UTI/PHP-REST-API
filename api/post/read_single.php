38f494c8ce9078d342ee108b3ec51674d3ae7866
Samiur-UTI <allpraisethelord@gmail.com>
2021-03-08 23:19:20 +0600

    read single done

---
+++ api/post/read_single.php
@@ -0,0 +1,30 @@
+<?php 
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+  
+    include_once '../../config/Database.php';
+    include_once '../../models/Post.php';
+  
+    // Instantiate DB & connect
+    $database = new Database();
+    $db = $database->connect();
+  
+    // Instantiate blog post object
+    $post = new Post($db);
+
+    //Get ID 
+    $post->id = isset($_GET['id']) ? $_GET['id']:die();
+    //Get post
+    $post->read_single();
+    //Create array
+    $post_arr = array('id' => $post->id,
+    'title'=> $post->title,
+    'body' => $post->body,
+    'category_id' => $post->category_id,
+    'category_name' => $post->category_name
+    );
+    //Make json
+    $result = json_encode($post_arr);
+    var_dump(json_decode($result) -> title);
+
+?>
